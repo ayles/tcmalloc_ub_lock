1fc4a380ce78e867198cef7d31d0199b7ae8a227
Alexander <selya.main@gmail.com>
2021-03-26 00:56:51 +0300

    minimal example

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+#include <thread>
+#include <cstdlib>
+#include <iostream>
+
 int main() {
+    void* mem = nullptr;   
+
+    auto f = [&mem]() {
+        while (true) {
+            void* m = mem;
+            mem = malloc(4096);
+            free(m);
+            std::cout << "here\n";
+        }
+    };
+
+    std::thread t(f);
+    f();
+
     return 0;
 }
