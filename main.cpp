d0df005b08c50193806b929acd733e92700b08ef
Alexander <selya.main@gmail.com>
2021-03-26 00:58:53 +0300

    added counter

--- main.cpp
+++ main.cpp
@@ -5,17 +5,20 @@
 int main() {
     void* mem = nullptr;   
 
-    auto f = [&mem]() {
+    auto f = [&mem](bool print) {
+        size_t i = 0;
         while (true) {
             void* m = mem;
             mem = malloc(4096);
             free(m);
-            std::cout << "here\n";
+            if (print) {
+                std::cout << i++ << std::endl;
+            }
         }
     };
 
-    std::thread t(f);
-    f();
+    std::thread t(f, false);
+    f(true);
 
     return 0;
 }
